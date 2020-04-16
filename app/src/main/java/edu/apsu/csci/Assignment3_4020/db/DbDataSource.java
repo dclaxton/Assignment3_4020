882a52859ac0048d2b313adbdec7cd3ed0efeb21
pwhite15 <pwhite15@my.apsu.edu>
2020-04-16 17:46:14 -0500

    added db

---
+++ app/src/main/java/edu/apsu/csci/Assignment3_4020/db/DbDataSource.java
@@ -0,0 +1,8 @@
+package edu.apsu.csci.Assignment3_4020.db;
+
+public class DbDataSource {
+
+
+
+
+}
---
+++ app/src/main/java/edu/apsu/csci/Assignment3_4020/db/MySqlLiteHelper.java
@@ -0,0 +1,63 @@
+package edu.apsu.csci.Assignment3_4020.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MySqlLiteHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "highscores.sqlite";
+    private static final int DB_VERSION = 1;
+    static final String DATA_TABLE = "Data";
+
+    // DB columns
+    public enum HighscoreColumns
+    {
+        primary_key,
+        simon_says,
+        player_adds,
+        simon_rewind;
+
+        public static String[] names()
+        {
+            HighscoreColumns[] v = values();
+            String[] names = new String[v.length];
+            for(int i = 0; i < v.length;i++)
+            {
+                names[i] = v[i].toString();
+            }
+            return names;
+        }
+    }
+
+
+
+    MySqlLiteHelper(Context context)
+    {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        // Create table with created columns
+        String sql = "CREATE TABLE " + DATA_TABLE + " (" +
+                HighscoreColumns.primary_key + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                HighscoreColumns.simon_says + " TEXT , " +
+                HighscoreColumns.player_adds + " TEXT ," +
+                HighscoreColumns.simon_rewind + " TEXT  " +
+                ");";
+
+        db.execSQL(sql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
